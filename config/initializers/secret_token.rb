0d2e1c377f5d6d7f6237ac1926f50a3574397bb7
leonardo.venoso <leonardo.venoso@live.com>
2014-10-10 02:36:00 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreeRubyOnRailsHtmlCssTemplate::Application.config.secret_key_base = '15d60ab8cf9696cb7613e99a81077bf34ed6cbe3ec5a607e63ac9603b3aa9bf119ecc68bbe6149d5eb4dea8785a7d6e9d181fbead8e0ff6478651c8899aef14b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FreeRubyOnRailsHtmlCssTemplate::Application.config.session_store :cookie_store, key: '_free_ruby_on_rails_html_css_template_session'
